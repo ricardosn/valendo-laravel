07d9c34dccb03fd3e68f821d2f1292aa9fe888bc
Ricardo da Silva Nunes <ricardorichsn@gmail.com>
2019-01-24 23:19:19 -0200

    fix migration

--- database/migrations/2019_01_02_222555_create_authors_table.php
+++ database/migrations/2019_01_02_222555_create_authors_table.php
@@ -15,19 +15,19 @@ class CreateAuthorsTable extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('name');
-            $table->text('known_as');
+            $table->string('name');
+            $table->string('known_as');
             $table->enum('gender', ['masculino', 'feminino', 'não binário', 'queer']);
-            $table->text('birthday');
-            $table->text('death');
-            $table->text('country');
-            $table->text('state');
-            $table->text('city');
-            $table->text('biography');
-            $table->text('site');
-            $table->text('facebook');
-            $table->text('twitter');
-            $table->text('wikipedia');
+            $table->date('birthday');
+            $table->date('death');
+            $table->string('country');
+            $table->char('state', 2);
+            $table->string('city');
+            $table->longText('biography');
+            $table->string('site');
+            $table->string('facebook');
+            $table->string('twitter');
+            $table->string('wikipedia');
             $table->timestamps();
         });
     }
